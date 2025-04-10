00fb28e51343f450b97135bf1e89756b55925957
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-04-10 14:41:28 +0100

    dictonary using dictionaryapi

---
+++ Dictionary/src/js.js
@@ -0,0 +1,45 @@
+const input = document.querySelector("input");
+const btn = document.querySelector(".btn");
+const word = document.querySelector(".word");
+const sound = document.querySelector("audio");
+const type = document.querySelector(".type");
+const spill = document.querySelector(".spill");
+const mean = document.querySelector(".mean");
+const show = document.querySelector(".show");
+const play = document.querySelector(".play");
+
+const fetchWord = (input) => {
+  if (!input.value.trim()) return;
+
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`, {
+    headers: { Accept: "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        show.style.display = "flex";
+        word.textContent = data[0].word;
+        if (data[0].phonetics[0]?.audio) {
+          sound.src = data[0].phonetics[0].audio;
+          play.style.display = "block";
+        } else {
+          play.style.display = "none";
+        }
+
+        type.textContent = data[0].meanings[0].partOfSpeech || "";
+        spill.textContent =
+          data[0].phonetics[0].text || "No phonetic available";
+        const firstDefinition = data[0].meanings[0]?.definitions[0];
+        mean.textContent =
+          firstDefinition?.example ||
+          firstDefinition?.definition ||
+          "No definition found";
+      } else {
+        show.innerHTML = "no word was found!";
+      }
+    });
+};
+btn.addEventListener("click", () => fetchWord(input));
+play.addEventListener("click", () => {
+  sound.play();
+});
